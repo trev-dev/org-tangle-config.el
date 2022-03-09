ec406f458447e8f0661226477ec2fb724be7317a
Trevor Richards <trev@trevdev.ca>
2022-03-08 21:48:00 -0800

    pull version back to beta

--- org-tangle-config.el
+++ org-tangle-config.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) Trevor Richards
 
 ;; Author: Trevor Richards <trev@trevdev.ca>
-;; Version: 1.0.2
+;; Version: 0.3.0
 ;; Keywords: performance, utility
 ;; URL: https://github.com/trev-dev/org-tangle-config.el
 
